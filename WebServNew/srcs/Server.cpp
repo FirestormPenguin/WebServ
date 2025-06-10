9a4413a2b9abec279be5c9f3eab13bb590f3ef8f
egiubell <edoardogiubellini@gmail.com>
2025-06-10 13:01:53 +0200

    comi

--- WebServNew/srcs/Server.cpp
+++ WebServNew/srcs/Server.cpp
@@ -39,15 +39,25 @@ Server::~Server() {
 }
 
 void Server::run() {
-	fd_set activeFds, readFds;
-	int maxFd = listenFd;
-
-	FD_ZERO(&activeFds);
-	FD_SET(listenFd, &activeFds);
+	fd_set readFds;
+	fd_set writeFds;
+	int maxFd;
 
 	while (true) {
-		readFds = activeFds;
+		FD_ZERO(&readFds);
+		FD_ZERO(&writeFds);
+		FD_SET(listenFd, &readFds);
+		maxFd = listenFd;
 
+		// Aggiungiamo tutti i client
+		std::map<int, Client*>::iterator it;
+		for (it = clients.begin(); it != clients.end(); ++it) {
+			int clientFd = it->first;
+			FD_SET(clientFd, &readFds);
+			maxFd = (clientFd > maxFd) ? clientFd : maxFd;
+		}
+
+		// Aspettiamo attività
 		if (select(maxFd + 1, &readFds, NULL, NULL, NULL) < 0) {
 			perror("select");
 			break;
@@ -59,63 +69,51 @@ void Server::run() {
 			if (newFd >= 0) {
 				std::cout << "New client connected: " << newFd << std::endl;
 				clients[newFd] = new Client(newFd);
-				FD_SET(newFd, &activeFds);
-				if (newFd > maxFd)
-					maxFd = newFd;
 			} else {
 				perror("accept");
 			}
 		}
 
-		// Itera sui client
-		std::map<int, Client*>::iterator it = clients.begin();
-		while (it != clients.end()) {
+		// Dati da client
+		for (it = clients.begin(); it != clients.end(); ) {
 			int clientFd = it->first;
 			Client* client = it->second;
 
 			if (FD_ISSET(clientFd, &readFds)) {
-				char buffer[1024];
-				std::string bufferStr;
-				ssize_t bytesRead;
+				char buffer[4096];
+				ssize_t bytesRead = recv(clientFd, buffer, sizeof(buffer), 0);
 
-				do {
-					memset(buffer, 0, sizeof(buffer));
-					bytesRead = recv(clientFd, buffer, sizeof(buffer), 0);
-					if (bytesRead > 0)
-						bufferStr.append(buffer, bytesRead);
-				} while (bytesRead > 0);
-
-				if (bufferStr.empty()) {
-					std::cout << "Client disconnected: " << clientFd << std::endl;
+				if (bytesRead <= 0) {
+					std::cout << "Closing client " << clientFd << std::endl;
 					close(clientFd);
 					delete client;
-					FD_CLR(clientFd, &activeFds);
+					clients.erase(it++);
+					continue;
+				} else {
+					client->appendToRecvBuffer(std::string(buffer, bytesRead));
+
+					std::string body = "Hello World\n";
+					std::ostringstream oss;
+					oss << "HTTP/1.1 200 OK\r\n"
+						<< "Content-Type: text/plain\r\n"
+						<< "Content-Length: " << body.length() << "\r\n"
+						<< "Connection: close\r\n"
+						<< "\r\n"
+						<< body;
+					std::string response = oss.str();
+
+					// Invio della risposta completa
+					send(clientFd, response.c_str(), response.size(), 0);
+
+					// Chiudo il client
+					std::cout << "Client " << clientFd << " served, closing." << std::endl;
+					close(clientFd);
+					delete client;
 					clients.erase(it++);
 					continue;
 				}
-
-				client->appendToRecvBuffer(bufferStr);
-
-				// Risposta semplice
-				std::string body = "Hello World\r\n";
-				std::ostringstream oss;
-				oss << "HTTP/1.1 200 OK\r\n"
-					<< "Content-Type: text/plain\r\n"
-					<< "Content-Length: " << body.length() << "\r\n"
-					<< "Connection: close\r\n"
-					<< "\r\n"
-					<< body;
-				std::string response = oss.str();
-
-				send(clientFd, response.c_str(), response.length(), 0);
-
-				close(clientFd);
-				delete client;
-				FD_CLR(clientFd, &activeFds);
-				clients.erase(it++);
-			} else {
-				++it;
 			}
+			++it;
 		}
 	}
 }
